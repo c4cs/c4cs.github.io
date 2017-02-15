d841122caf2c0b38373e64709dba64f1fbac112a
nlparis <nlparis@umich.edu>
2017-02-14 19:17:02 -0500

    Update anchor.js

--- javascript/anchor.js
+++ javascript/anchor.js
@@ -1,16 +1,16 @@
-//$(function() {
-  //$('a[href*="#"]:not([href="#"])').click(function() {
-    //if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      //var target = $(this.hash);
-      //target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      //if (target.length) {
-        //var scrollToPosition = $(target).offset().top - 50;
-        //$('html').animate({ 'scrollTop': scrollToPosition }, 600);
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        var scrollToPosition = $(target).offset().top - 50;
+        $('html').animate({ 'scrollTop': scrollToPosition }, 600);
         //$('html, body').animate({
-        //  scrollTop: target.offset().top
-        //}, 1000);
-        //return false;
-      //}
-    //}
-  //});
-//});
+         // scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
