0943a419e8a79ff9fdbb75f7136b2fd4ac8dace0
nlparis <nlparis@umich.edu>
2017-02-15 13:48:43 -0500

    Update nlparis.js

--- static/js/nlparis.js
+++ static/js/nlparis.js
@@ -4,7 +4,7 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
       $('html, body').animate({
-        scrollTop: target.offset().top
+        scrollTop: target.offset().top - 50
       }, 1000);
       return false;
     }
