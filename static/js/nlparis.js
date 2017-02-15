be5c2124e6289c9d56f492e425eab474c4206b76
nlparis <nlparis@umich.edu>
2017-02-15 13:46:42 -0500

    Update nlparis.js

--- static/js/nlparis.js
+++ static/js/nlparis.js
@@ -1,1 +1,12 @@
-alert("hi its me nick paris");
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
