e0b36ccec024d389c62d7379b049086b8d8833fe
nlparis <nlparis@umich.edu>
2017-02-14 19:07:04 -0500

    Update anchor.js

--- javascript/anchor.js
+++ javascript/anchor.js
@@ -1,6 +1,6 @@
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    //if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       //var target = $(this.hash);
       //target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
@@ -11,6 +11,6 @@ $(function() {
         //}, 1000);
         return false;
       }
-    }
+    //}
   });
 });
