13b74cbeee41c04b895ed376c4bbeb9d92250990
nlparis <nlparis@umich.edu>
2017-02-14 19:00:19 -0500

    Update anchor.js

--- javascript/anchor.js
+++ javascript/anchor.js
@@ -4,11 +4,11 @@ $(function() {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
-        var scrollToPosition = $(target).offset().top - 50;
-        $('html').animate({ 'scrollTop': scrollToPosition }, 600);
+        //var scrollToPosition = $(target).offset().top - 50;
+        //$('html').animate({ 'scrollTop': scrollToPosition }, 600);
         //$('html, body').animate({
         //  scrollTop: target.offset().top
-        }, 1000);
+        //}, 1000);
         return false;
       }
     }
