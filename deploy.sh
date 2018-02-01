6ac35138f8b3c33890c5269f7e9cf740c4f62013
Tarun Khubchandani <tarun@kfit.com>
2018-02-01 01:17:21 -0500

    Attempt to fix branch issue

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
+set -e
+
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_files() {
-  git checkout -b master
+  git checkout -b build-branch
   git add .
   git commit --allow-empty -m "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/c4cs/c4cs.github.io.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages master
+  git push --quiet --set-upstream origin-pages $TRAVIS_BRANCH
 }
 
 setup_git
